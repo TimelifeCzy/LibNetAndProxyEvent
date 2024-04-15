ae66ad8e3284613405b763de8bec78af8687bc45
czy <timelifezero@163.com>
2024-04-15 18:20:52 +0800

    ebpf libs

--- LinuxNet/build/clean.sh
+++ LinuxNet/build/clean.sh
@@ -1,5 +1,5 @@
-rm -rf CMakeCache.txt
-rm -rf cmake_install.cmake
-rm -rf CMakeFiles
-rm -rf Makefile
-rm -rf unixpory
+rm -rf ./CMakeCache.txt
+rm -rf ./cmake_install.cmake
+rm -rf ./CMakeFiles
+rm -rf ./Makefile
+rm -rf ./unixpory
